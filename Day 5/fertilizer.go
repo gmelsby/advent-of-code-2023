e6841a7c5207934eac1900da8ea82f378dafec1f
Gregory Melsby <greg27426@gmail.com>
2023-12-08 13:27:28 -0500

    inefficient solution created

--- Day 5/fertilizer.go
+++ Day 5/fertilizer.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -52,9 +53,7 @@ func main() {
 		}
 	}
 
-	fmt.Println(seeds)
-	fmt.Println(mapList)
-
+	fmt.Println(solveLowestLocation(seeds, mapList))
 	readFile.Close()
 }
 
@@ -70,4 +69,24 @@ func addLineValuesToMap(line string, valueMap map[int]int) {
 	for idx := 0; idx < nums[2]; idx++ {
 		valueMap[nums[1]+idx] = nums[0] + idx
 	}
+}
+
+func solveLowestLocation(seeds []int, mapList []map[int]int) int {
+	locations := []int{}
+	for _, currentVal := range seeds {
+		for _, currentMap := range mapList {
+			currentVal = getWithDefaultValue(currentMap, currentVal)
+		}
+		locations = append(locations, currentVal)
+	}
+	return slices.Min(locations)
+}
+
+// returns value if key in map, returns key if key not in map
+func getWithDefaultValue(currentMap map[int]int, key int) int {
+	val, ok := currentMap[key]
+	if !ok {
+		return key
+	}
+	return val
 }

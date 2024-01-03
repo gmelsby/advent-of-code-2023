4ce186723e22f991203cff5ccc66f2215fb3f491
Gregory Melsby <greg27426@gmail.com>
2024-01-03 14:13:44 -0500

    Day 15 part 2 complete

--- Day 15/main.go
+++ Day 15/main.go
@@ -4,12 +4,15 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
 	result1 := solveHashPuzzle("input.txt")
 	fmt.Println(result1)
+	result2 := solveHashMapPuzzle("input.txt")
+	fmt.Println(result2)
 }
 
 func solveHashPuzzle(input string) int {
@@ -28,6 +31,72 @@ func solveHashPuzzle(input string) int {
 	result := 0
 	for _, step := range steps {
 		result += hashAlgorithm(step)
+	}
+	return result
+}
+
+type reflector = struct {
+	label       string
+	focalLength *int
+}
+
+func solveHashMapPuzzle(input string) int {
+	readFile, err := os.Open(input)
+	if err != nil {
+		panic(err)
+	}
+	defer readFile.Close()
+
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Scan()
+	line := fileScanner.Text()
+
+	steps := strings.Split(line, ",")
+
+	hashMap := map[int]*[]reflector{}
+	for _, step := range steps {
+		if strings.HasSuffix(step, "-") {
+			label := strings.TrimSuffix(step, "-")
+			boxAddr, ok := hashMap[hashAlgorithm(label)]
+			if !ok {
+				continue
+			}
+			box := *boxAddr
+			for i, reflector := range box {
+				if reflector.label == label {
+					newBox := append(box[:i], box[i+1:]...)
+					hashMap[hashAlgorithm(label)] = &newBox
+					break
+				}
+			}
+		} else {
+			label, focalLengthStr, _ := strings.Cut(step, "=")
+			focalLength, _ := strconv.Atoi(focalLengthStr)
+			boxAddr, ok := hashMap[hashAlgorithm(label)]
+			if !ok {
+				boxAddr = &[]reflector{}
+				hashMap[hashAlgorithm(label)] = boxAddr
+			}
+			box := *boxAddr
+			foundFlag := false
+			for _, reflector := range box {
+				if reflector.label == label {
+					*(reflector.focalLength) = focalLength
+					foundFlag = true
+					break
+				}
+			}
+			if !foundFlag {
+				newBox := append(box, reflector{label, &focalLength})
+				hashMap[hashAlgorithm(label)] = &newBox
+			}
+		}
+	}
+	result := 0
+	for box, contents := range hashMap {
+		for i, lens := range *contents {
+			result += (box + 1) * (i + 1) * (*lens.focalLength)
+		}
 	}
 	return result
 }
